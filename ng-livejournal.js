c6494e7bf8cee71204380ef1b988722e2ceb6100
leoz2011 <leoz@yahoo.com>
2014-10-21 20:59:44 -0400

    Restructure function calls

--- ng-livejournal.js
+++ ng-livejournal.js
@@ -15,101 +15,111 @@
 
 		var URL = 'http://www.livejournal.com/interface/xmlrpc';
 		
-		return {
-	
-			post: function(params,cbGood,cbFail,context) {
-				$http({
-					method: 'POST',
-					url: URL,
-					data: params,
-					headers: {
-						'Content-Type': 'text/xml'
-					}
-				}).
-				success(function(data, status) {
-					var xmlDoc = x2js.parseXmlString(data);
-				
-					var response;
-				
-		            try {
-						response = XMLRPC.parseDocument(xmlDoc);
-						cbGood(response,context);
-		            }
-		            catch(err) {
-						cbFail(context);
-		            }				
-				
-				}).
-				error(function(data, status) {
-				});
-			},		
-			prepare_call: function(method,params) {
-				var xmlDoc = XMLRPC.document(method, [params]);
-				var data;
-				if ("XMLSerializer" in window) {
-					data = new window.XMLSerializer().serializeToString(xmlDoc);
-				} else {
-					// IE does not have XMLSerializer
-					data = xmlDoc.xml;
+		function postCall(params,cbGood,cbFail,context) {
+			$http({
+				method: 'POST',
+				url: URL,
+				data: params,
+				headers: {
+					'Content-Type': 'text/xml'
 				}
-				return data;
-			},
-			array_buffer_to_string: function(buf) {
-				var d = $q.defer();
-				var bb = new Blob([buf]);
-				var f = new FileReader();
-				f.onload = function(e) {
-					d.resolve(e.target.result);
+			}).
+			success(function(data, status) {
+				var xmlDoc = x2js.parseXmlString(data);
+			
+				var response;
+			
+				try {
+					response = XMLRPC.parseDocument(xmlDoc);
+					cbGood(response,context);
 				}
-				f.readAsText(bb);
-				return d.promise;
-			},		
-			get_userpics: function(user,cbGood,cbFail,context) {
-				var method = 'LJ.XMLRPC.getuserpics';
-				var params = {
-					'ver'        : '1',
-					'usejournal' : user
-				};        
-				var param = this.prepare_call(method,params);         
-				this.post(param,cbGood,cbFail,context);
-			},
-			get_events: function(count,journal,last_date,cbGood,cbFail,context) {
-		        var method = 'LJ.XMLRPC.getevents';
-			    var params = {
-				        'ver'        : '1',
-				        'selecttype' : 'lastn',
-				        'howmany'    : count,
-		                'usejournal' : journal
-			    };
-			    if (last_date) {
-				    params['beforedate'] = last_date;
-			    }   
-		        var param = this.prepare_call(method,params);         
-		        this.post(param,cbGood,cbFail,context);     
-			},
-			get_event: function(journal,itemid,cbGood,cbFail,context) {
-		        var method = 'LJ.XMLRPC.getevents';
-			    var params = {
-				        'ver'        : '1',
-				        'selecttype' : 'one',
-				        'itemid'     : itemid,
-		                'usejournal' : journal
-			    };
-		        var param = this.prepare_call(method,params);         
-		        this.post(param,cbGood,cbFail,context);     
-			},		
-			get_comments: function(itemid,anum,journal,cbGood,cbFail,context) {
-		        var method = 'LJ.XMLRPC.getcomments';
-		        var ditemid = itemid * 256 + anum;
-			    var params = {
-					'ver'     : '1',
-					'journal' : journal,
-					'ditemid' : ditemid
-			    };  
-		        var param = this.prepare_call(method,params);         
-		        this.post(param,cbGood,cbFail,context);     
+				catch(err) {
+					cbFail(context);
+				}				
+			
+			}).
+			error(function(data, status) {
+			});
+		};
+		
+		function prepareCall(method,params) {
+			var xmlDoc = XMLRPC.document(method, [params]);
+			var data;
+			if ("XMLSerializer" in window) {
+				data = new window.XMLSerializer().serializeToString(xmlDoc);
+			} else {
+				// IE does not have XMLSerializer
+				data = xmlDoc.xml;
 			}
-	
+			return data;
+		};
+		
+		function arrayBufferToString(buf) {
+			var d = $q.defer();
+			var bb = new Blob([buf]);
+			var f = new FileReader();
+			f.onload = function(e) {
+				d.resolve(e.target.result);
+			}
+			f.readAsText(bb);
+			return d.promise;
+		};
+		
+		function getUserpics(user,cbGood,cbFail,context) {
+			var method = 'LJ.XMLRPC.getuserpics';
+			var params = {
+				'ver'        : '1',
+				'usejournal' : user
+			};        
+			var param = prepareCall(method,params);         
+			postCall(param,cbGood,cbFail,context);
+		};
+		
+		function getEvents(count,journal,last_date,cbGood,cbFail,context) {
+			var method = 'LJ.XMLRPC.getevents';
+			var params = {
+					'ver'        : '1',
+					'selecttype' : 'lastn',
+					'howmany'    : count,
+					'usejournal' : journal
+			};
+			if (last_date) {
+				params['beforedate'] = last_date;
+			}   
+			var param = prepareCall(method,params);         
+			postCall(param,cbGood,cbFail,context);     
+		};
+		
+		function getEvent(journal,itemid,cbGood,cbFail,context) {
+			var method = 'LJ.XMLRPC.getevents';
+			var params = {
+					'ver'        : '1',
+					'selecttype' : 'one',
+					'itemid'     : itemid,
+					'usejournal' : journal
+			};
+			var param = prepareCall(method,params);         
+			postCall(param,cbGood,cbFail,context);     
+		};
+		
+		function getComments(itemid,anum,journal,cbGood,cbFail,context) {
+			var method = 'LJ.XMLRPC.getcomments';
+			var ditemid = itemid * 256 + anum;
+			var params = {
+				'ver'     : '1',
+				'journal' : journal,
+				'ditemid' : ditemid
+			};  
+			var param = prepareCall(method,params);         
+			postCall(param,cbGood,cbFail,context);     
+		};
+
+		return {
+			array_buffer_to_string : arrayBufferToString,
+			get_userpics           : getUserpics,
+			get_events             : getEvents,
+			get_event              : getEvent,
+			get_comments           : getComments
 		};
 	}]);
 })();
